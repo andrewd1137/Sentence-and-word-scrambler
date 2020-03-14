6792fda95d14165b006e9461024b5dd3beffad8c
Andrew DeMarco <60309182+andrewd1137@users.noreply.github.com>
2020-03-14 12:09:03 -0400

    Update randStr.py

--- randStr.py
+++ randStr.py
@@ -2,14 +2,17 @@
 
 import random
 
+# dictionary for 3rd function
 Int4Word = {}
 
 
+# returns a random word from a sentence
 def randWord(str1):
     split_string = str1.split()
     return random.choice(split_string)
 
 
+# mixes words in a sentence or letters in a word (depending on length of input)
 def strMixer(str1):
     split_string = str1.split()
     if len(split_string) == 1:
@@ -19,6 +22,7 @@ def strMixer(str1):
     return shuffle_string
 
 
+# assigns a random int value for a word and stores it in the dictionary
 def randIntForWord(char1):
     if char1 in Int4Word.keys():
         return Int4Word[char1]
